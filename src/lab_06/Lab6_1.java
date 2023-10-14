314baffca919938ea936f9404dad8aab4a882819
gam.d <gam.d@stor.ai>
2023-10-14 09:27:23 +0700

    end day 2

--- src/lab_06/Lab6_1.java
+++ src/lab_06/Lab6_1.java
@@ -6,9 +6,20 @@ public class Lab6_1 {
          *
          Given input string: "2hrs and 5 minutes"
          Please calculate how many minutes in total
+         tinh ra so gio : 2
+         chia ra gio va hut
+         tinh ra so phut: 5
          */
 
-String inputString = "2hrs and 5 minutes";
+        String inputString = "2hrs and 5 minutes";
+        String[] hourAndMinutes = inputString.split("and");
+        String hourString = hourAndMinutes[0].replaceAll("[^0-9]","");
+        String minuteString = hourAndMinutes[1].replaceAll("[^0-9]", "");
 
+        int hour = Integer.parseInt(hourString);
+        int minute = Integer.parseInt(minuteString);
+
+        int totalMinutes = hour*60 + minute;
+        System.out.println("total minutes is " + totalMinutes);
     }
 }
